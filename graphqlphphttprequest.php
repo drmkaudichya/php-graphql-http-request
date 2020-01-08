e49507e3e0d58f31aecaef24a5e3e4836555c7e6
Milind Audichya <milindaudichya@gmail.com>
2020-01-08 11:12:58 +0530

    Update graphqlphphttprequest.php
    Cleaned Code

--- graphqlphphttprequest.php
+++ graphqlphphttprequest.php
@@ -1,35 +1,29 @@
 <?php
 function graphql_query(string $endpoint, string $query, array $variables = [], ?string $token = null): array
 {
-	//$headers = array();
-    //$headers = ['Content-Type: application/json', 'Accept: application/json'];
 	$headers = array();
 	$headers[] = 'Content-Type: application/json';
 	$headers[] = 'Accept: application/graphql';
 	$headers[] = 'Host: example.com'; //Hostname here
-    if (null !== $token) {
-        $headers[] = "Authorization: Bearer $token";
-    }
-	//print_r($headers);
-	//echo "<br/>";
+	if (null !== $token) 
+	{
+		$headers[] = "Authorization: Bearer $token";
+	}
 	
-    if (false === $data = @file_get_contents($endpoint, false, stream_context_create([
+	if (false === $data = @file_get_contents($endpoint, false, stream_context_create([
         'http' => [
             'method' => 'POST',
             'header' => $headers,
             'content' => json_encode(['query' => $query, 'variables' => $variables]),
-        ]
-    ]))) {
-        $error = error_get_last();
-        throw new \ErrorException($error['message'], $error['type']);
-    }
-    return json_decode($data, true);
+        ]]))) 
+	{
+        	$error = error_get_last();
+        	throw new \ErrorException($error['message'], $error['type']);
+    	}
+    	return json_decode($data, true);
 }
 
-
-
 $query = 'query login{}'; //GraphQL Query Here
-
 $data = graphql_query('https://example.com/graphql', $query, [], null);
 //print_r($data);
 
@@ -45,5 +39,4 @@ $query = 'query apps {
 
 $data = graphql_query('https://example.com/graphql', $query, [], $token);
 print_r($data);
-
 ?>
